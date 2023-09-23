e24ad2c4995af7662ccec9fdebd046e72119fe1f
Carlos Zegarra <cgzegarra@gmail.com>
2023-09-23 14:10:04 -0400

    first commit

---
+++ src/main/java/com/simplilearn/course/servlet/WelcomeServlet.java
@@ -0,0 +1,37 @@
+package com.simplilearn.course.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class WelcomeServlet
+ */
+
+@WebServlet("/WelcomeServlet")
+public class WelcomeServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        PrintWriter out = response.getWriter();
+        out.println("Hello");
+        out.println("My");
+        out.println("Name");
+        out.println("Is");
+        out.println("Simplilearn");
+        out.println("Thanks for reaching us !");
+        out.println("Have a nice day :) ");
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
+    }
+
+}
